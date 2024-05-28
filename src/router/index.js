b04ff0587d4b7f24c1ffd15aa7c8cc0c4f5a54f9
Majidkhanliver <62938113+Majidkhanliver@users.noreply.github.com>
2024-05-29 00:02:42 +0530

    vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import CoachDetail from '@/pages/coaches/CoachDetail.vue';
-import CoachRegister from '@/pages/coaches/CoachRegister.vue';
-import CoachesList from '@/pages/coaches/CoachesList.vue';
-import ContactCoach from '@/pages/requests/ContactCoach.vue';
-import RequestReceived from '@/pages/requests/RequestReceived.vue';
-import NotFound from '@/pages/NotFound.vue';
-import UserAuth from '@/pages/auth/UserAuth.vue';
-import store from '../store/index.js'
+import { createRouter, createWebHistory } from "vue-router";
+import { defineAsyncComponent } from "vue";
+// import CoachDetail from '@/pages/coaches/CoachDetail.vue';
+// import CoachRegister from '@/pages/coaches/CoachRegister.vue';
+import CoachesList from "@/pages/coaches/CoachesList.vue";
+// import ContactCoach from '@/pages/requests/ContactCoach.vue';
+// import RequestReceived from '@/pages/requests/RequestReceived.vue';
+import NotFound from "@/pages/NotFound.vue";
+// import UserAuth from '@/pages/auth/UserAuth.vue';
+import store from "../store/index.js";
+const CoachDetail = () => import("@/pages/coaches/CoachDetail.vue");
+const CoachRegister = () => import("@/pages/coaches/CoachRegister.vue");
+const ContactCoach = () => import("@/pages/requests/ContactCoach.vue");
+const RequestReceived = () => import("@/pages/requests/RequestReceived.vue");
+const UserAuth = () => import("@/pages/auth/UserAuth.vue");
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -21,7 +27,7 @@ const router = createRouter({
     {
       path: "/coaches/:id",
       component: CoachDetail,
-      props:true,
+      props: true,
       children: [
         {
           path: "contact",
@@ -31,15 +37,18 @@ const router = createRouter({
     },
     {
       path: "/register",
-      component: CoachRegister,meta:{requiresAuth:true}
+      component: CoachRegister,
+      meta: { requiresAuth: true },
     },
     {
       path: "/requests",
-      component: RequestReceived,meta:{requiresAuth:true}
+      component: RequestReceived,
+      meta: { requiresAuth: true },
     },
     {
-      path: '/auth',
-      component:UserAuth,meta:{requiresUnAuth:true}
+      path: "/auth",
+      component: UserAuth,
+      meta: { requiresUnAuth: true },
     },
     {
       path: "/:notFound(.*)",
@@ -48,15 +57,14 @@ const router = createRouter({
   ],
 });
 router.beforeEach(function (to, _, next) {
-  debugger
+  debugger;
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
-    next('/auth');
+    next("/auth");
   }
   if (to.meta.requiresUnAuth && store.getters.isAuthenticated) {
     next("/coaches");
   } else {
     next();
   }
-  
-})
-export default router
+});
+export default router;
